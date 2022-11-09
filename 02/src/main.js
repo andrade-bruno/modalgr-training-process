712ebc582c0da8d8a8b3d96a0cae5e40c786da6c
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-11-09 11:29:54 -0300

    feat: integratin color picker with Huebee

--- 02/src/main.js
+++ 02/src/main.js
@@ -16,3 +16,15 @@ function doHighlight() {
 button.addEventListener('click', () => {
     doHighlight()
 })
+
+const colorInput = document.querySelector('.color-input');
+const backgroundEditor = document.getElementById('texteditor')
+
+var hueb = new Huebee( colorInput, {
+  setBGColor: true,
+  saturations: 2,
+});
+
+hueb.on('change', function( color, hue, sat, lum ) {
+    backgroundEditor.style.backgroundColor = color
+})
