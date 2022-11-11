59bd91c803dbefb68c75d6c8e084d0ad8eeec9a1
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-11-11 09:44:35 -0300

    fix: moved assets folder to public dir

---
+++ react-desenvolvendo-com-javascript/organo/src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+export default function Banner() {
+    return (
+        <>
+            <header className='banner'>
+                <img src="/assets/banner.png" alt='Bannerr'/>
+            </header>
+        </>
+    )
+}
