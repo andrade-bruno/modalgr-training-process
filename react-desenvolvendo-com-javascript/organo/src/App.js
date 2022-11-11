b9c88e19c726a532d33d23de3a36bfc37a6ab710
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-11-11 15:30:55 -0300

    feat: react-toastify notification

--- react-desenvolvendo-com-javascript/organo/src/App.js
+++ react-desenvolvendo-com-javascript/organo/src/App.js
@@ -3,12 +3,16 @@ import Banner from './components/Banner';
 import Form from './components/Form';
 import './main.css';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
     const [teammates, setTeammates] = useState([]);
 
     const handleSubmit = teammate => {
         setTeammates([...teammates, teammate]);
         console.log('Colaboradores:', teammates);
+        toast(`${teammate.name} cadastrado`);
     };
 
     return (
@@ -16,6 +20,15 @@ function App() {
             <div className='app'>
                 <Banner />
                 <Form onSubmit={handleSubmit} />
+                <ToastContainer
+                    position='top-center'
+                    autoClose={2500}
+                    closeButton={false}
+                    hideProgressBar={false}
+                    rtl={false}
+                    draggable
+                    theme='dark'
+                />
             </div>
         </>
     );
