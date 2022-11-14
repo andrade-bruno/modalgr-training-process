c03bce62ec9d2e78c99d75c9917ec58693442658
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-11-14 15:53:52 -0300

    feat: theme.js

--- styled-components/src/Components/Cabecalho/index.jsx
+++ styled-components/src/Components/Cabecalho/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import bank_logo from "../../assets/images/bank_logo.svg";
+import styled from 'styled-components'
+import theme from "../UI/theme";
+
+const Header = styled.nav`
+  background-color: ${theme.colors.primary};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`
 
 const Cabecalho = () => {
   return (
-    <div className="cabecalho">
+    <Header>
       <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
       <div>
         <a className="btn-secundario" href="https://google.com">
@@ -13,7 +24,7 @@ const Cabecalho = () => {
           Sair
         </a>
       </div>
-    </div>
+    </Header>
   );
 };
 
---
+++ styled-components/src/Components/UI/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    primary: '#41D3BE',
+  },
+  background: {
+    light: '#F1F1F1',
+  },
+  text: {
+    gray: 'grey',
+  }
+  
+}
+
+export default theme;
