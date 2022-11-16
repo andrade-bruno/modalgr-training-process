45fe92923e726497a89338620a9d6de28068e240
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-11-16 14:24:37 -0300

    feat: initilized CustomLink

--- react-router/src/components/Menu/index.js
+++ react-router/src/components/Menu/index.js
@@ -1,13 +1,26 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import styles from './menu.module.css'
 
 export default function Menu() {
     return (
         <header>
             <nav className={styles.navegacao}>
-                <Link className={styles.link} to="/">Home</Link>
-                <Link className={styles.link} to="/about">Sobre</Link>
+                <CustomLink path="/">Home</CustomLink>
+                <CustomLink path="/about">Sobre</CustomLink>
             </nav>
         </header>
+    )
+}
+
+const CustomLink = ({children, path}) => {
+    const location = useLocation()
+
+    return (
+        <Link to={path} className={`
+            ${styles.link}
+            ${location.pathname === path ? styles.currentLink : null}
+        `}>
+            {children}
+        </Link>
     )
 }
