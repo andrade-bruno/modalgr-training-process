f6155fb7aacbf808403fed39cf9d8ef72e16f629
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-11-16 14:02:47 -0300

    feat: initialized react router

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,6 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './pages/about';
+import Home from './pages/home';
+
 function App() {
   return (
-    <>React Router</>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='*' element={<div>Page not found</div>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
