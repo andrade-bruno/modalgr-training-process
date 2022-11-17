fa3a526f1964c77fae8ed9f4407b214c9f15400b
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-11-17 14:10:29 -0300

    refactor: PostPage not found page validation

--- react-router/src/pages/Post/index.js
+++ react-router/src/pages/Post/index.js
@@ -1,9 +1,11 @@
-import { useParams } from "react-router-dom"
+import { Route, Routes, useParams } from "react-router-dom"
 
 import './post.css'
 import posts from 'assets/json/posts.json'
 import PostModel from "components/PostModel"
 import ReactMarkdown from "react-markdown"
+import NotFound from "pages/NotFound"
+import DefaultPage from "components/DefaultPage"
 
 export default function Post() {
     const params = useParams()
@@ -14,22 +16,26 @@ export default function Post() {
 
     if (!currentPost) {
         return (
-            <div className="post-not-found">
-                <p>Post not found</p>
-            </div>
+            <NotFound />
         )
     }
 
     return (
-        <PostModel
-            coverPhoto={require(`../../assets/posts/${currentPost.id}/cover.png`)}
-            title={currentPost.title}
-        >
-            <div className="post-markdown-container">
-                <ReactMarkdown>
-                    {currentPost.content}
-                </ReactMarkdown>
-            </div>
-        </PostModel>
+        <Routes>
+            <Route path='*' element={<DefaultPage />}>
+                <Route index element={
+                    <PostModel
+                        coverPhoto={require(`../../assets/posts/${currentPost.id}/cover.png`)}
+                        title={currentPost.title}
+                    >
+                        <div className="post-markdown-container">
+                            <ReactMarkdown>
+                                {currentPost.content}
+                            </ReactMarkdown>
+                        </div>
+                    </PostModel>
+                } />
+            </Route>
+        </Routes>
     )
 }
