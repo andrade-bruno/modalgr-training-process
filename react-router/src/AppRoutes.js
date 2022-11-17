17e4235616ac17fdaae67ed5de6684fef854d73a
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-11-17 10:18:57 -0300

    feat: Outlet router dom

--- react-router/src/AppRoutes.js
+++ react-router/src/AppRoutes.js
@@ -3,6 +3,7 @@ import About from './pages/about';
 import Home from './pages/home';
 import Menu from './components/Menu'
 import Footer from 'components/Footer';
+import DefaultPage from 'components/DefaultPage';
 
 function AppRoutes() {
   return (
@@ -10,8 +11,11 @@ function AppRoutes() {
       <Menu />
 
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/about' element={<About />}/>
+        <Route path='/' element={<DefaultPage />}>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+        </Route>
+
         <Route path='*' element={<div>Page not found</div>}/>
       </Routes>
 
--- react-router/src/pages/home.js
+++ react-router/src/pages/home.js
@@ -1,4 +1,3 @@
-import Banner from '../components/Banner/index';
 import styles from './home.module.css'
 
 import posts from '../assets/json/posts.json'
@@ -6,16 +5,12 @@ import Post from '../components/Post';
 
 export default function Home() {
     return (
-        <main>
-            <Banner />
-            
-            <ul className={styles.posts}>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <Post post={post} />
-                    </li>
-                ))}
-            </ul>
-        </main>
+        <ul className={styles.posts}>
+            {posts.map((post) => (
+                <li key={post.id}>
+                    <Post post={post} />
+                </li>
+            ))}
+        </ul>
     )
 }
