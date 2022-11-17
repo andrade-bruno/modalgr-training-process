d07231406a43c33d4c2b4bf8f7876660df21567a
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-11-17 12:21:50 -0300

    feat: post-markdown-container css

--- react-router/src/pages/Post/index.js
+++ react-router/src/pages/Post/index.js
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom"
+
+import './post.css'
 import posts from 'assets/json/posts.json'
 import PostModel from "components/PostModel"
 import ReactMarkdown from "react-markdown"
@@ -15,9 +17,11 @@ export default function Post() {
             coverPhoto={require(`../../assets/posts/${currentPost.id}/cover.png`)}
             title={currentPost.title}
         >
-            <ReactMarkdown>
-                {currentPost.content}
-            </ReactMarkdown>
+            <div className="post-markdown-container">
+                <ReactMarkdown>
+                    {currentPost.content}
+                </ReactMarkdown>
+            </div>
         </PostModel>
     )
 }
