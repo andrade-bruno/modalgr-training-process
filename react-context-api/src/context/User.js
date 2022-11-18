b4e72713da3d9b84f01347aafcbd26c136e08072
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-11-18 11:49:20 -0300

    feat: Consuming UserContext inside Feira component

--- react-context-api/src/context/User.js
+++ react-context-api/src/context/User.js
@@ -1,4 +1,15 @@
 import React from 'react'
 
 export const UserContext = React.createContext();
+UserContext.displayName = "User"
 
+export const UserProvider = ({ children }) => {
+    const [name, setName] = React.useState('')
+    const [balance, setBalance] = React.useState(0)
+
+    return (
+        <UserContext.Provider value={{ name, setName, balance, setBalance }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
--- react-context-api/src/routes.js
+++ react-context-api/src/routes.js
@@ -4,24 +4,19 @@ import Login from 'pages/Login'
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { UserContext } from 'context/User'
+import { UserProvider } from 'context/User'
 
 function Router() {
-    const [name, setName] = React.useState('')
-    const [balance, setBalance] = React.useState(0)
-
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' exact element={
-                    <UserContext.Provider value={{ name, setName, balance, setBalance }}>
-                        <Login />
-                    </UserContext.Provider>
-                } />
-                <Route path='/feira' element={<Feira />} />
-                <Route path='/carrinho' element={<Carrinho />} />
-            </Routes>
-        </BrowserRouter>
+        <UserProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' exact element={<Login />} />
+                    <Route path='/feira' element={<Feira />} />
+                    <Route path='/carrinho' element={<Carrinho />} />
+                </Routes>
+            </BrowserRouter>
+        </UserProvider>
     )
 }
 
