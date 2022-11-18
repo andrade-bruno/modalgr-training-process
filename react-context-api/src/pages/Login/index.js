af305b20e23542a13decd30fd4cb49a8633fba38
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-11-18 11:16:21 -0300

    feat: UserContext

--- react-context-api/src/pages/Login/index.js
+++ react-context-api/src/pages/Login/index.js
@@ -12,46 +12,54 @@ import {
 
 import { useNavigate } from 'react-router-dom'
 
-export default function Login({ name, setName, balance, setBalance }) {
+import { UserContext } from 'context/User';
+
+export default function Login() {
   const navigate = useNavigate();
 
   return (
-    <Container>
-      <Titulo>
-        Insira o seu nome
-      </Titulo>
-      <InputContainer>
-        <InputLabel>
-          Nome
-        </InputLabel>
-        <Input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </InputContainer>
-      <InputContainer>
-        <InputLabel>
-          Saldo
-        </InputLabel>
-        <Input
-          type="number"
-          value={balance}
-          onChange={(e) => setBalance(e.target.value)}
-          startAdornment={
-            <InputAdornment position="start">
-              R$
-            </InputAdornment>
-          }
-        />
-      </InputContainer>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => { navigate('/feira') }}
-      >
-        Avançar
-      </Button>
-    </Container>
+    <UserContext.Consumer>
+      {({ name, setName, balance, setBalance }) => (
+        <>
+          <Container>
+            <Titulo>
+              Insira o seu nome
+            </Titulo>
+            <InputContainer>
+              <InputLabel>
+                Nome
+              </InputLabel>
+              <Input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </InputContainer>
+            <InputContainer>
+              <InputLabel>
+                Saldo
+              </InputLabel>
+              <Input
+                type="number"
+                value={balance}
+                onChange={(e) => setBalance(e.target.value)}
+                startAdornment={
+                  <InputAdornment position="start">
+                    R$
+                  </InputAdornment>
+                }
+              />
+            </InputContainer>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => { navigate('/feira') }}
+            >
+              Avançar
+            </Button>
+          </Container>
+        </>
+      )}
+    </UserContext.Consumer>
   )
 };
--- react-context-api/src/routes.js
+++ react-context-api/src/routes.js
@@ -4,6 +4,8 @@ import Login from 'pages/Login'
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import { UserContext } from 'context/User'
+
 function Router() {
     const [name, setName] = React.useState('')
     const [balance, setBalance] = React.useState(0)
@@ -12,7 +14,9 @@ function Router() {
         <BrowserRouter>
             <Routes>
                 <Route path='/' exact element={
-                    <Login name={name} setName={setName} balance={balance} setBalance={setBalance} />
+                    <UserContext.Provider value={{ name, setName, balance, setBalance }}>
+                        <Login />
+                    </UserContext.Provider>
                 } />
                 <Route path='/feira' element={<Feira />} />
                 <Route path='/carrinho' element={<Carrinho />} />
