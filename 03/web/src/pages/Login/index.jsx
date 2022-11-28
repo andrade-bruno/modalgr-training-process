aceec7b8772021cbaf9ddbfcd5491bf20f14fcfd
Bruno Andrade <bruno.andrade@modalgr.com.br>
2022-11-28 13:57:39 -0300

    feat: handleLogin login page

--- 03/web/src/pages/Login/index.jsx
+++ 03/web/src/pages/Login/index.jsx
@@ -4,14 +4,21 @@ import Input from 'components/Input';
 import Button from 'components/Button'
 
 import { Container } from './styles';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login(params) {
+    const navigate = useNavigate()
+
+    const handleLogin = () => {
+        navigate('/')
+    }
+
     return (
         <Container>
             <p><b>Iniciar Sessão</b></p>
             <Input type='email' placeholder='Escreva seu email' mobileW="80%" tabletW="300px" desktopW="420px" />
             <Input type='password' placeholder='Escreva sua senha' mobileW="80%" tabletW="300px" desktopW="420px" />
-            <Button mobileW="50%" tabletW="110px" desktopW="420px">Entrar</Button>
+            <Button mobileW="50%" tabletW="110px" desktopW="420px" onClick={handleLogin}>Entrar</Button>
         </Container>
     )
 };
