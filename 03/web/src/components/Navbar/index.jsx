9ef08e4d804f1e50d5976b3925f2af42c7a80c5a
Bruno Andrade <bruno.andrade@modalgr.com.br>
2022-11-28 13:54:11 -0300

    feat: initialized Navbar navigation button

--- 03/web/src/components/Navbar/index.jsx
+++ 03/web/src/components/Navbar/index.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import { NavbarStyle, SearchBar, SearchInput, Img, SearchIconMobile } from './styles';
+import { NavbarStyle, SearchBar, SearchInput, Img, SearchIconMobile, LeftContent, ButtonNavigate } from './styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import theme from 'theme';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-export default function Navbar(params) {
+export default function Navbar(props) {
+    const location = useLocation()
+    const pathname = location.pathname
     const navigate = useNavigate()
+
+    const handleNavigateNavBar = () => {
+        return pathname === '/' ? navigate('/login') :
+            pathname === '/product/new' ? navigate('/products')
+                : null
+    }
 
     return (
         <NavbarStyle>
-            <Img src='/images/logo.png' alt='Alura Geek' onClick={() => navigate('/')} />
-            <SearchBar>
-                <SearchInput placeholder='O que deseja encontrar?' />
-                <FontAwesomeIcon icon="search" color={theme.fontColor.gray} size='lg' />
-            </SearchBar>
+            <LeftContent>
+                <Img src='/images/logo.png' alt='Alura Geek' onClick={() => navigate('/')} />
+                <SearchBar>
+                    <SearchInput placeholder='O que deseja encontrar?' />
+                    <FontAwesomeIcon icon="search" color={theme.fontColor.gray} size='lg' style={{ cursor: 'pointer' }} />
+                </SearchBar>
+            </LeftContent>
+            {pathname === '/' ? <ButtonNavigate onClick={handleNavigateNavBar}>Login</ButtonNavigate> :
+                pathname === '/product/new' ? <ButtonNavigate onClick={handleNavigateNavBar}>Menu administrador</ButtonNavigate>
+                    : null}
             <SearchIconMobile icon="search" color={theme.fontColor.gray} size='lg' />
         </NavbarStyle>
     )
--- 03/web/src/components/Navbar/styles.js
+++ 03/web/src/components/Navbar/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import theme from 'theme'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from 'components/Button';
 
 const navHeight = '7.125rem'
 
@@ -9,21 +10,39 @@ export const NavbarStyle = styled.div`
     display: flex;
     height: ${navHeight};
     align-items: center;
-    justify-content: flex-start;
+    justify-content: space-between;
     width: 100%;
-    gap: 32px;
 
     @media (min-width: 768px) {
         padding: 10px 152px;
+        gap: 32px;
     }
     @media (max-width: 768px) {
         padding: 10px 32px;
+        gap: 32px;
     }
     @media (max-width: 360px) {
         padding: 10px 16px;
+        gap: 16px;
         justify-content: space-between;
     }
+`
 
+export const LeftContent = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 32px;
+
+    @media (min-width: 768px) {
+        gap: 32px;
+    }
+    @media (max-width: 768px) {
+        gap: 32px;
+    }
+    @media (max-width: 360px) {
+        gap: 16px;
+    }
 `
 
 export const Img = styled.img`
@@ -58,10 +77,10 @@ export const SearchBar = styled.div`
     gap: 20px;
 
     @media (min-width: 768px) {
-        width: 394px;
+        max-width: 394px;
     }
     @media (max-width: 768px) {
-        width: 272px;
+        max-width: 272px;
     }
     @media (max-width: 360px) {
         display: none;
@@ -74,10 +93,38 @@ export const SearchInput = styled.input`
     border: none;
     width: 100%;
     color: ${theme.fontColor.gray};
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`
+
+export const ButtonNavigate = styled(Button)`
+    background: transparent;
+    color: ${theme.fontColor.primary};
+    border: 1px solid ${theme.background.blue};
+    box-shadow: none;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+    @media (min-width: 768px) {
+        font-size: 16px;
+    }
+    @media (max-width: 768px) {
+        font-size: 14px;
+    }
+    @media (max-width: 360px) {
+        font-size: 14px;
+    }
+
+    &:hover {
+        box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
+    }
 `
 
 export const SearchIconMobile = styled(FontAwesomeIcon)`
     display: none;
+    cursor: pointer;
 
     @media (max-width: 360px) {
         display: block;
