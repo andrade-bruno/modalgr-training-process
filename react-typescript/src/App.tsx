a04ff1536c77fca1c88265d3dc501250a13fd7db
Bruno Andrade <bruno.andrade@modalgr.com.br>
2022-11-29 16:03:38 -0300

    feat(882): initialized form and button component

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Form from 'components/Form';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+          <Form />
+        </div>
+    );
 }
 
 export default App;
