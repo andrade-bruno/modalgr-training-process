fcabfc2be8f03d5fbfa1d70f3d48b43381e50bfc
Bruno Andrade <bruno.andrade@modalgr.com.br>
2022-11-30 11:13:59 -0300

    refactor(884): liststyles refactored to module sass

--- react-typescript/src/components/List/index.tsx
+++ react-typescript/src/components/List/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style.scss'
+import style from './List.module.scss'
  
 const List = () => {
     let tasks = [
@@ -14,11 +14,11 @@ const List = () => {
     ]
 
     return (
-        <aside className='taskList'>
+        <aside className={style.taskList}>
             <h2>Estudos do dia</h2>
             <ul>
                 {tasks.map((item, index) => (
-                    <li key={index} className='item'>
+                    <li key={index} className={style.item}>
                         <h3>{item.title}</h3>
                         <span>{item.time}</span>
                     </li>
