ab47780cced7517dd2eb95d1c05794e1014fb6ab
Bruno Andrade <bruno.andrade@modalgr.com.br>
2022-11-30 10:37:05 -0300

    feat(884): Button styles

--- react-typescript/src/components/Button/index.tsx
+++ react-typescript/src/components/Button/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './style.scss'
 class Button extends React.Component  {
     render() {
         return (
-            <button>Botão</button>
+            <button className='mainButton'>Botão</button>
         )
     }
 }
