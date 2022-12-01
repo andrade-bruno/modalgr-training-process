611be3aef061a5e18e6eb352d43fe3837464a351
Bruno Andrade <bruno.andrade@modalgr.com.br>
2022-12-01 14:56:10 -0300

    refactor(888): changed Button to functional component

--- react-typescript/src/components/Button/index.tsx
+++ react-typescript/src/components/Button/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import style from './Button.module.scss'
-class Button extends React.Component<{ 
+
+interface ButtonProps {
     children: any,
     type?: "button" | "submit" | "reset" | undefined ,
     onClick?: () => void
-}>  {
-    render() {
-        const { type = 'button', children, onClick } = this.props
-        return (
-            <button className={style.mainButton} onClick={onClick} type={type}>{children}</button>
-        )
-    }
+}
+
+const Button = (props: ButtonProps) => {
+    const { type = 'button', children, onClick } = props
+    return (
+        <button className={style.mainButton} onClick={onClick} type={type}>{children}</button>
+    )
 }
 
 export default Button
