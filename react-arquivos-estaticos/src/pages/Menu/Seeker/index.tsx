e9fa3747030c3465bdbb065488fefecf88d01f39
Bruno Andrade <bruno.andrade@modalgr.com.br>
2022-12-02 10:45:57 -0300

    feat(942): Seeker, _breakpoints and colors - sass vars

---
+++ react-arquivos-estaticos/src/pages/Menu/Seeker/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import styles from './Seeker.module.scss'
+import { CgSearch } from 'react-icons/cg'
+
+interface Props {
+    search: string,
+    setSearch: React.Dispatch<React.SetStateAction<string>>
+}
+
+const Seeker = ({search, setSearch}: Props) => {
+    return (
+        <div className={styles.seeker}>
+            <input
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+            />
+            <CgSearch size={20} color='#4C4D5F' />
+        </div>
+    )
+}
+
+export default Seeker
--- react-arquivos-estaticos/src/pages/Menu/index.tsx
+++ react-arquivos-estaticos/src/pages/Menu/index.tsx
@@ -1,10 +1,15 @@
+import React from 'react'
+
 import styles from './Menu.module.scss'
 import {ReactComponent as Logo} from 'assets/logo.svg'
+import Seeker from './Seeker'
 
 const Menu = () => {
+    const [search, setSearch] = React.useState('')
+
     return (
         <main>
-            <nav className={styles.menu}>
+            <nav className={styles.nav}>
                 <Logo />
             </nav>
             <header className={styles.header}>
@@ -12,6 +17,10 @@ const Menu = () => {
                     A casa do código e da massa
                 </div>
             </header>
+            <section className={styles.menu}>
+                <h3 className={styles.menu__title}>Cardápio</h3>
+                <Seeker search={search} setSearch={setSearch} />
+            </section>
         </main>
     )
 }
