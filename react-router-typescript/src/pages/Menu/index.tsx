ae8aec242dc99f7950ff577283520a2b7adbede7
Bruno Andrade <bruno.andrade@modalgr.com.br>
2022-12-05 14:48:10 -0300

    feat(990): created Header component

--- react-router-typescript/src/pages/Menu/index.tsx
+++ react-router-typescript/src/pages/Menu/index.tsx
@@ -13,22 +13,15 @@ const Menu = () => {
     const [sorter, setSorter] = React.useState('')
 
     return (
-        <main>
-            <header className={styles.header}>
-                <div className={styles.header__text}>
-                    A casa do código e da massa
-                </div>
-            </header>
-            <section className={styles.menu}>
-                <h3 className={styles.menu__title}>Cardápio</h3>
-                <Seeker search={search} setSearch={setSearch} />
-                <div className={styles.menu__filters}>
-                    <Filters filter={filter} setFilter={setFilter} />
-                    <Sorter sorter={sorter} setSorter={setSorter} />
-                </div>
-                <Itens search={search} filter={filter} sorter={sorter} />
-            </section>
-        </main>
+        <section className={styles.menu}>
+            <h3 className={styles.menu__title}>Cardápio</h3>
+            <Seeker search={search} setSearch={setSearch} />
+            <div className={styles.menu__filters}>
+                <Filters filter={filter} setFilter={setFilter} />
+                <Sorter sorter={sorter} setSorter={setSorter} />
+            </div>
+            <Itens search={search} filter={filter} sorter={sorter} />
+        </section>
     )
 }
 
--- react-router-typescript/src/routes/main.tsx
+++ react-router-typescript/src/routes/main.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from 'pages/Home'
 import Menu from 'pages/Menu'
 import Navbar from 'components/Navbar'
+import Header from 'components/Header'
 
 const MainRoutes = () => {
     return (
         <BrowserRouter>
             <Navbar />
+            <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/menu' element={<Menu />} />
