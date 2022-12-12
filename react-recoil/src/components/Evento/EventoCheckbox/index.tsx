7596c157e7b87b5f01f418cd271487eaf877caf7
Bruno Andrade <bruno.andrade@modalgr.com.br>
2022-12-12 15:39:16 -0300

    refactor(1076): removed prop drilling for deleting and changing event

--- react-recoil/src/components/Evento/EventoCheckbox/index.tsx
+++ react-recoil/src/components/Evento/EventoCheckbox/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { IEvento } from '../../../interfaces/IEvento'
+import { useSetRecoilState } from 'recoil'
+import { listaDeEventosStates } from './../../../state/atom'
 
-const EventoCheckbox: React.FC<{ evento: IEvento, aoAlterarStatus: (id: number) => void }> = ({ evento, aoAlterarStatus }) => {
-  
+const EventoCheckbox: React.FC<{ evento: IEvento }> = ({ evento }) => {
+	const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosStates)
+
+	const alterarStatus = () => {
+		evento.completo = !evento.completo
+
+		setListaDeEventos(prev => {
+			const idx = prev.findIndex(item => item.id === evento.id)
+			return [...prev.slice(0, idx), evento, ...prev.slice(idx + 1)]
+		})
+	}
+
 	const estilos = [
 		'far',
 		'fa-2x',
 		evento.completo ? 'fa-check-square' : 'fa-square'
 	]
 
-	return (<i className={estilos.join(' ')} onClick={() => aoAlterarStatus(evento.id!)}></i>)
+	return (<i className={estilos.join(' ')} onClick={alterarStatus}></i>)
 }
 
 export default EventoCheckbox
--- react-recoil/src/components/ListaDeEventos/index.tsx
+++ react-recoil/src/components/ListaDeEventos/index.tsx
@@ -6,11 +6,8 @@ import { listaDeEventosStates } from './../../state/atom'
 import { useRecoilValue } from 'recoil'
 
 const ListaDeEventos: React.FC<{ 
-	aoAlterarStatus: (id: number) => void, 
-	aoDeletarEvento: (id: number) => void, 
 	aoFiltroAplicado: (data: Date | null) => void
-}> = ({ aoDeletarEvento, aoAlterarStatus, aoFiltroAplicado }) => {
-
+}> = ({ aoFiltroAplicado }) => {
 	const eventos = useRecoilValue(listaDeEventosStates)
 
 	return (
@@ -18,7 +15,7 @@ const ListaDeEventos: React.FC<{
 			<Filtro aoFiltroAplicado={aoFiltroAplicado} />
 			<div className={style.Scroll}>
 				{eventos.map(evento => (
-					<Evento aoAlterarStatus={aoAlterarStatus} aoDeletarEvento={aoDeletarEvento} evento={evento} key={evento.id} />
+					<Evento evento={evento} key={evento.id} />
 				))}
 			</div>
 		</section>
