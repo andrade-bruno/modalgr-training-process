2fb9ad2dedeb2549017127143210af4b038447ac
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-12-22 21:19:44 -0300

    chore(1115): documentation pdf sample

--- 04/src/pages/Docs/index.tsx
+++ 04/src/pages/Docs/index.tsx
@@ -1,6 +1,11 @@
+import { Object } from './styles'
+
 const Docs = () => {
 	return (
-		<h1>Documentação</h1>
+		<>
+			<h1>Documentação</h1>
+			<Object data="/assets/documentation.pdf" type="application/pdf" />
+		</>
 	)
 }
  
