356fc94274fa00882a31f195cdbd2a374e95c205
Bruno Andrade <bruno.andrade@modalgr.com.br>
2023-01-02 14:40:16 -0300

    feat(1187): Login submit controller

--- 04/src/pages/SignIn/index.tsx
+++ 04/src/pages/SignIn/index.tsx
@@ -1,43 +1,50 @@
-import Input from 'components/Input'
-import { useState } from 'react'
 import { Button, Typography } from '@mui/material'
 import { Box, Container, SignInSignUpForm, Logo } from 'styles/commom'
 import { BackgroundImg } from './styles'
 import { useUserContext } from 'contexts/UserContext'
 import { Link } from 'react-router-dom'
+import { ControlledInput, Errors } from 'components/ControlledInput/styles'
+import { FieldValues, useForm } from 'react-hook-form'
 
 const SignIn = () => {
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-
 	const { login } = useUserContext()
+	const { register, handleSubmit, formState: { errors } } = useForm()
 	
-	const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
-		await login(email, password)
+	const handleLogin = async (e: FieldValues) => {
+		await login(e.email, e.password)
 	}
 
 	return (
 		<Container>
 			<Box>
-				<SignInSignUpForm style={{justifyContent: 'center'}} onSubmit={e => handleLogin(e)}>
+				<SignInSignUpForm style={{justifyContent: 'center'}} onSubmit={handleSubmit(e => handleLogin(e))}>
 					<Logo src='/assets/logo.png' alt='Logo' />
-					<Input
-						label='E-mail'
-						value={email}
-						setter={setEmail}
+					<ControlledInput
+						{...register('email',{
+							required: 'Campo obrigatório',
+							maxLength: {value: 128, message: 'Máximo de 128 caracteres'},
+							pattern: {value: /^\w+([.-]?\w+)*@modalgr.com.br$/, message: 'E-mail inválido'}
+						})}
+						error={errors.email ? true : false}
 						type='email'
+						label='E-mail'
 						fullWidth
 						required
 					/>
-					<Input
-						label='Senha'
-						value={password}
-						setter={setPassword}
+					<Errors>{errors.email && `${errors.email.message}`}</Errors>
+					<ControlledInput
+						{...register('password',{
+							required: 'Campo obrigatório',
+							minLength: {value: 6, message: 'Mínimo de 6 caracteres'},
+							maxLength: {value: 128, message: 'Máximo de 128 caracteres'},
+						})}
+						error={errors.password ? true : false}
 						type='password'
+						label='Senha'
 						fullWidth
 						required
 					/>
+					<Errors>{errors.password && `${errors.password.message}`}</Errors>
 					<Typography variant='overline' component='h6'>
 						Ainda não utiliza a plataforma? <Link style={{ textDecoration: 'none' }} to='/signup'>Cadastre-se.</Link>
 					</Typography>
